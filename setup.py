fb72a13f0320965e87c9ba2b60a0bf773a84a8c2
Bartev Vartanian <bartev@apple.com>
2021-06-23 00:20:22 -0500

    Init commit cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Coursera course on sports analytics',
+    author='Bartev',
+    license='MIT',
+)
